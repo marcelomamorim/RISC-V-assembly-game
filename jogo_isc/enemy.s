b70ba6851e134196805993cce56e9d757dbff08a
Pedro Henrique Dos Santos <devpredohs@gmail.com>
2020-12-17 02:08:35 -0300

    Adds the player two finalization

--- jogo_isc/enemy.s
+++ jogo_isc/enemy.s
@@ -10,7 +10,7 @@
 	jr t0
 
 T_DIFFICULTY_1: # 1ST DAN - 2ND DAN
-	DIFFICULTY_1()
+	DIFFICULTY_3()
 	la t0,END
 	jr t0
 
@@ -97,11 +97,24 @@ T_ATQ:
 	jr t0
 P2_SOCO:	
 	PUNCH_P2()
+	DEFEAT_P1()
+	FINISH_P2()
+	addi s7,s7,1
+	YIN_YANG()
+
+	
 
 	la t0,P2_FIM
 	jr t0
 P2_CHUTE:
 	KICK_P2()
+	DEFEAT_P1()
+	FINISH_P2()
+	addi s7,s7,1
+	YIN_YANG()
+	
+	
+	
 	la t0,P2_FIM
 	jr t0
 	# se nao estiver perto o suficiente, anda na direcao do player
@@ -110,15 +123,31 @@ P2_ANDA:blt s5,s1,T_P2_DIR
 	jr, t0
 	
 T_P2_DIR:
-	la t0,P2_DIR
+	la t0,ENCERRA
 	jr, t0
 	
 P2_ESQ:	WALK_P2_ESQ()
-	la t0,P2_FIM
+	la t0,ENCERRA
 	jr, t0
 P2_DIR: WALK_P2_DIR()
 
+
 P2_FIM: 
+	li t0,4
+	bgeu s7,t0,T1_GAME_OVER
+	
+	la t0,P2_RESET
+	jr t0
+	
+T1_GAME_OVER:
+	la t0,T2_GAME_OVER
+	jr t0
+	
+P2_RESET:
+	la t0,RESET
+	jr, t0
+ENCERRA:
+	
 .end_macro
 
 .macro DIFFICULTY_3()
@@ -136,11 +165,21 @@ T_ATQ:
 	jr t0
 P2_SOCO:	
 	PUNCH_P2()
-	la t0,P2_FIM
+	DEFEAT_P1()
+	FINISH_P2()
+	addi s7,s7,1
+	YIN_YANG()
+	
+	la t0,FIM
 	jr t0
 P2_CHUTE:
 	KICK_P2()
-	la t0,P2_FIM
+	DEFEAT_P1()
+	FINISH_P2()
+	addi s7,s7,1
+	YIN_YANG()
+	
+	la t0,FIM
 	jr t0
 	# se nao estiver perto o suficiente, anda na direcao do player
 P2_ANDA:blt s5,s1,T_P2_DIR
@@ -161,7 +200,7 @@ P2_FIM:
 	rem t1,s0,t0
 	bnez t1,P2_PULO
 
-	la t0,FIM
+	la t0,ENCERRA
 	jr, t0
 P2_PULO:	
 	beqz s6,PULO_DIR
@@ -169,9 +208,25 @@ P2_PULO:
 	jr, t0
 PULO_DIR:
 	JUMP_P2_RIGHT()
-	la t0,FIM
+	la t0,ENCERRA
 	jr, t0
 PULO_ESQ:	
 	JUMP_P2_LEFT()
-FIM:
+	la t0,ENCERRA
+	jr, t0
+FIM: 
+	li t0,4
+	bgeu s7,t0,T1_GAME_OVER
+	
+	la t0,P2_RESET
+	jr t0
+	
+T1_GAME_OVER:
+	la t0,T2_GAME_OVER
+	jr t0
+	
+P2_RESET:
+	la t0,RESET
+	jr, t0
+ENCERRA:
 .end_macro
